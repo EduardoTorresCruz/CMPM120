644d4903f31f5b90a95a9553c6a454ae16d7cc2d
EduardoTorresCruz <73498664+EduardoTorresCruz@users.noreply.github.com>
2023-05-01 18:00:30 -0700

    Finished studio scene

--- README.md
+++ README.md
@@ -1,11 +1,15 @@
 Key requirements for this project:
 - **3+ scenes**:
+    - Studio Scene - First scene where the "game logo" is introduced an animation plays along with a little jingle
 - **2+ audio assets**:
+    - Chime - Played during the studio scene with the game studio logo
 - **2+ image assets**: 
     - TheNarrowRoadLogo.png(Game Title) - An image png created in Adobe Illustrator. The text is one built in Illustrator with some gradient behind it, then some manipulation of shapes created the road and crown
     - LuciousLightLogo.png(Studio Logo) - An image png created in Adobe Illustrator. The text is one built in Illustrator with some slight modifications such as border, opacity, etc which is combined with a simple light bulb for the i in "light" created with geometric shapes
 - **2+ geometric shapes**:
+    - Star - The star that is animated over the studio logo created in-built with Phaser
 - **1+ logotype**:
 - **1+ multi-line text**:
 - **1+ fading animation**:
+    - Transition between studio scene to game scene - Fade to white
 - **1+ object motion animation**:
--- script.js
+++ script.js
@@ -13,24 +13,68 @@ class StudioScene extends Phaser.Scene {
         this.graphics = this.add.graphics();
 
         // add fullscreen button
-        let fullscreen_button = this.add.image(475, 25, 'fullscreen_img',).setInteractive();
-        fullscreen_button.setScale(0.1) // resize to 10% of original size
+        let fullscreen_img = this.add.image(475, 25, 'fullscreen_img',).setInteractive();
+        fullscreen_img.setScale(0.1); // resize to 10% of original size
 
-        fullscreen_button.on('pointerup', function(){
+        // functionality of the fullscreen button
+        fullscreen_img.on('pointerup', function(){
             if(!this.scale.isFullscreen){
                 this.scale.startFullscreen();
             }
         }, this);
 
         // add studio logo
-        this.imageObject = this.add.image(
-            275, // x
-            250, // y
-            'studio_logo',
-        );
-        this.imageObject.setScale(0.33) // resize to 33% of original size
+        let studio_logo = this.add.image(275, 250, 'studio_logo',);
+        studio_logo.setScale(0.33); // resize to 33% of original size
 
+        // add transition
+        this.graphics.fillStyle(0xFFFFFF, 1); // color, opacity
+        let white_rect = this.graphics.fillRect(0, 0, 500, 500);
 
+        studio_logo.alpha = 0; 
+        fullscreen_img.alpha = 0; 
+        white_rect.alpha = 0;
+        white_rect.depth = 10; // ensures rectangle is on top of everything
+
+        // have the screen fade in for 4 sceonds
+        this.tweens.add({
+            targets: [studio_logo, fullscreen_img],
+            alpha: 1,
+            duration: 4000,
+            ease: 'Linear',
+            repeat: 0,
+        });
+
+        // Delay this event by 4 seconds
+        this.time.addEvent({
+            delay: 4000,
+            callback: ()=>{
+                // create star 
+                let star = this.add.star(200, 200, 5, 3, 6, 0xFFFFFF);
+            
+                // have the star pop in
+                this.tweens.add({
+                targets: star,
+                scaleX: 3,
+                scaleY: 3,
+                angle: 300,
+                duration: 2000,
+                ease: 'Bounce',
+                repeat: 0,
+                });
+            },
+            loop: false,
+        });
+
+        // have the screen fade out to white(1 second after all animations)
+        this.tweens.add({
+            targets: white_rect,
+            delay: 7000,
+            alpha: 1,
+            duration: 1000,
+            ease: 'Linear',
+            repeat: 0,
+        });
     }
     update(){
 
