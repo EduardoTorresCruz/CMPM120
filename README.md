5536d97e42602c64f0cc6f0b0ba41de03b5c3a44
EduardoTorresCruz <73498664+EduardoTorresCruz@users.noreply.github.com>
2023-05-03 00:50:19 -0700

    finish product

--- README.md
+++ README.md
@@ -2,9 +2,14 @@ Key requirements for this project:
 - **3+ scenes**:
     - Studio Scene - First scene where the "game logo" is introduced an animation plays along with a little jingle
     - Game Scene - Second scene showcasing a preview of the game(a stickman and the mountain he has to climb)
+    - Title Scene - Third and final scene with the game title and a list of text options with some background music
 - **2+ audio assets**:
     - ChimeSFX.m4a - Created via voice memos application on Mac via my hitting a metal cup
     - WarpSFX.m4a - Created via voice memos application on Mac via my personal voice
+    - ForTheFallen.mp3 - The following music was used for this media project:
+    Music: For the Fallen by Kevin MacLeod
+    Free download: https://filmmusic.io/song/3772-for-the-fallen
+    License (CC BY 4.0): https://filmmusic.io/standard-license
 - **2+ image assets**: 
     - TheNarrowRoadLogo.png(Game Title) - An image png created in Adobe Illustrator. The text is one built in Illustrator with some gradient behind it, then some manipulation of shapes created the road and crown
     - LuciousLightLogo.png(Studio Logo) - An image png created in Adobe Illustrator. The text is one built in Illustrator with some slight modifications such as border, opacity, etc which is combined with a simple light bulb for the i in "light" created with geometric shapes
@@ -12,8 +17,13 @@ Key requirements for this project:
     - MountainsAtNight.png(Background of game scene) - An image png created in Adobe Illustrator. Made with a gradient and geometric shapes to resembel a mountain
 - **2+ geometric shapes**:
     - Star - The star that is animated over the studio logo created in-built with Phaser
+    - Stickman - Stickman shown for a moment in the game scene
 - **1+ logotype**:
+    - LuciousLightLogo.png(Studio Logo) - An image png created in Adobe Illustrator. The text is one built in Illustrator with some slight modifications such as border, opacity, etc which is combined with a simple light bulb for the i in "light" created with geometric shapes
 - **1+ multi-line text**:
+    - Muli-line text in the game scene regarding the difficulty of the narrow road
 - **1+ fading animation**:
     - Transition between studio scene to game scene - Fade to white
 - **1+ object motion animation**:
+    - The background of the game scene sliding to the right via a tween
+    - Text on title scene
--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 class StudioScene extends Phaser.Scene {
-    // Gives scene unique label
+    // gives scene unique label
     constructor(){
         super('studio_scene');
     }
@@ -35,7 +35,7 @@ class StudioScene extends Phaser.Scene {
         let studio_logo = this.add.image(275, 250, 'studio_logo',);
         studio_logo.setScale(0.33); // resize to 33% of original size
 
-        // add transition
+        // add transition(first creating white rectangle)
         this.graphics.fillStyle(0xFFFFFF, 1); // color, opacity
         let white_rect = this.graphics.fillRect(0, 0, 500, 500);
 
@@ -53,7 +53,7 @@ class StudioScene extends Phaser.Scene {
             repeat: 0,
         });
 
-        // Delay this event by 4 seconds
+        // delay this event by 4 seconds
         this.time.addEvent({
             delay: 4000,
             callback: ()=>{
@@ -85,6 +85,7 @@ class StudioScene extends Phaser.Scene {
             loop: false,
         });
 
+        // delay this event by 7 seconds
         this.time.addEvent({
             delay: 7000,
             callback: ()=>{
@@ -103,7 +104,7 @@ class StudioScene extends Phaser.Scene {
             repeat: 0,
         });
 
-        // Go to game scene after all that needs to occur in this studio scene(8 seconds)
+        // go to game scene after all that needs to occur in this studio scene(8 seconds)
         this.time.delayedCall(8000, () => {
             this.scene.start('game_scene');
         })
@@ -114,7 +115,7 @@ class StudioScene extends Phaser.Scene {
 }
 
 class GameScene extends Phaser.Scene {
-    // Gives scene unique label
+    // gives scene unique label
     constructor(){
         super('game_scene');
     }
@@ -142,14 +143,61 @@ class GameScene extends Phaser.Scene {
             }
         }, this);
 
+        // adds stickman
+        this.graphics.fillStyle(0x000000, 1); // color, opacity
+        let stickman_head = this.graphics.fillCircle(25, 460, 10);
+        stickman_head.depth = 1;
+        let stickman_body = this.add.line(0,0, 25, 470, 25, 500, 0x000000, 1);
+        stickman_body.depth = 2;
+        let stickman_arms = this.add.line(0,0, 25, 480, 45, 480, 0x000000, 1);
+        stickman_body.depth = 3;
+        let stickman_leg1 = this.add.line(0,0, 25, 490, 23, 510, 0x000000, 1);
+        stickman_body.depth = 4;
+        let stickman_leg2 = this.add.line(0,0, 29, 510, 26, 490, 0x000000, 1);
+        stickman_body.depth = 4;
+
+        // multi-line text
+        let text = this.add.text(100, 50, "The road you're about to take is one of perseverance and suffering. Many will not find it and for the few that do, many will not see the end.");
+        text.setWordWrapWidth(300);
+
+        // slides the scene after a 3 seconds of delay
         this.tweens.add({
+            delay: 3000,
             targets: background_img,
-            duration: 4000,
+            duration: 8000,
             x: 130,
             ease: 'Linear',
             repeat: 0,
         });
 
+        // fades text away after a 4 second delay
+        this.tweens.add({
+            delay: 4000,
+            targets: text,
+            duration: 10000,
+            alpha: 0,
+            ease: 'Linear',
+            repeat: 0,
+        });
+        
+        // delay this event by 3 seconds
+        this.time.addEvent({
+            delay: 3500,
+            callback: ()=>{
+                // make stickman invisible
+                stickman_head.alpha = 0;
+                stickman_body.alpha = 0;
+                stickman_arms.alpha = 0;
+                stickman_leg1.alpha = 0;
+                stickman_leg2.alpha = 0;
+            },
+            loop: false,
+        });
+
+        // go to title scene after all that needs to occur in this studio scene(10 seconds)
+        this.time.delayedCall(10000, () => {
+            this.scene.start('title_scene');
+        })
     }
     update(){
 
@@ -164,9 +212,85 @@ class TitleScene extends Phaser.Scene {
     preload(){
         this.load.path = './assets/';
         this.load.image('game_title', 'TheNarrowRoadLogo.png');
+        this.load.audio('game_music', 'ForTheFallen.mp3');
     }
     create(){
         this.graphics = this.add.graphics();
+
+        // add fullscreen button
+        let fullscreen_img = this.add.image(475, 25, 'fullscreen_img',).setInteractive();
+        fullscreen_img.setScale(0.1); // resize to 10% of original size
+        fullscreen_img.alpha = 0.5;
+
+        // functionality of the fullscreen button
+        fullscreen_img.on('pointerup', function(){
+            if(!this.scale.isFullscreen){
+                this.scale.startFullscreen();
+            }
+        }, this);
+
+        // add brownish background
+        this.graphics.fillStyle(0xD3BBAF, 1); // color, opacity
+        let brown_rect = this.graphics.fillRect(0, 0, 500, 500);
+        brown_rect.depth = 0;
+
+        // add game title
+        let game_title = this.add.image(240, 100, 'game_title',);
+        game_title.setScale(0.1); // resize to 33% of original size
+
+        // create background audio loop
+        let background_music = this.sound.add('game_music', {volume: 0.3}, {loop: true});
+        background_music.loop = true;
+        background_music.play();
+
+        // create all text
+        let play_text = this.add.text(-300, 170, "Play", {
+            fontFamily: 'Ariel',
+            fontSize: '50px',
+            color: '#0x000000',
+        });
+        play_text.preFX.addShadow(0, 0, 0.2, 2, 0x333333, 5);
+        let options_text = this.add.text(-300, 270, "Options", {
+            fontFamily: 'Ariel',
+            fontSize: '50px',
+            color: '#0x000000',
+        });
+        options_text.preFX.addShadow(0, 0, 0.2, 2, 0x333333, 5);
+        let credits_text = this.add.text(-300, 370, "Credits", {
+            fontFamily: 'Ariel',
+            fontSize: '50px',
+            color: '#0x000000',
+        })
+        credits_text.preFX.addShadow(0, 0, 0.2, 2, 0x333333, 5);
+
+        // text sliding animations
+        this.tweens.add({
+            delay: 1000,
+            targets: play_text,
+            duration: 2000,
+            x: 80,
+            ease: 'Linear',
+            repeat: 0,
+        });
+
+        this.tweens.add({
+            delay: 3000,
+            targets: options_text,
+            duration: 2000,
+            x: 80,
+            ease: 'Linear',
+            repeat: 0,
+        });
+
+        this.tweens.add({
+            delay: 5000,
+            targets: credits_text,
+            duration: 2000,
+            x: 80,
+            ease: 'Linear',
+            repeat: 0,
+        });
+       
     }
     update(){
 
